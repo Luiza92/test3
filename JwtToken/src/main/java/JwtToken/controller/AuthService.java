66397c8fc26294499162a694cc8a845aba3333f3
Luiza <luiza.harutyunyan20@gmail.com>
2023-01-30 13:03:24 +0400

    add jwtToken

---
+++ JwtToken/src/main/java/JwtToken/controller/AuthService.java
@@ -0,0 +1,26 @@
+package JwtToken.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+public class Controller {
+
+    private final AuthService authService;
+
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("hello/user")
+    public ResponseEntity<String> helloUser() {
+        final JwtAuthentication authInfo = authService.getAuthInfo();
+        return ResponseEntity.ok("Hello user " + authInfo.getPrincipal() + "!");
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("hello/admin")
+    public ResponseEntity<String> helloAdmin() {
+        final JwtAuthentication authInfo = authService.getAuthInfo();
+        return ResponseEntity.ok("Hello admin " + authInfo.getPrincipal() + "!");
+    }
---
+++ JwtToken/src/main/java/JwtToken/model/Role.java
@@ -0,0 +1,17 @@
+package JwtToken.model;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role implements GrantedAuthority {
+
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String vale;
+
+    @Override
+    public String getAuthority() {
+        return vale;
+    }
+}
